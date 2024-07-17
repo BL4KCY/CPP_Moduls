fd3f084a0f8aaed45a082cd7561016bfa5aac287
BL4KCY <mostafafarssiui@gmail.com>
2024-07-17 20:36:33 +0100

    chore: Add menu options for adding, searching, and exiting in main.cpp

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -3,7 +3,20 @@
 
 int main(void)
 {
-	PhoneBook phoneb;
-
-	phoneb.add();
+	PhoneBook	phoneb;
+	std::string	input;
+	while (true)
+	{
+		std::cout << "Your options: \"ADD, SEARCH, EXIT\"" << std::endl;
+		std::cout << "Please Enter you option: ";
+		std::getline(std::cin, input);
+		if (input == "ADD")
+			phoneb.add();
+		else if (input == "SEARCH")
+			phoneb.search();
+		else if (input == "EXIT")
+			return (0);
+		else
+			std::cout << "Wrong optins!!" << std::endl;
+	}
 }
