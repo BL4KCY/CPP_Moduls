533fd9a598b9e0b20ed8d02749505771cc57be32
BL4KCY <mostafafarssiui@gmail.com>
2024-07-18 19:05:50 +0100

    refactor: Update main.cpp menu options and improve error handling

--- cpp00/ex01/main.cpp
+++ cpp00/ex01/main.cpp
@@ -7,9 +7,16 @@ int main(void)
 	std::string	input;
 	while (true)
 	{
-		std::cout << "Your options: \"ADD, SEARCH, EXIT\"" << std::endl;
+		std::cout << GREEN << "___________________COMMANDS___________________" << END << std::endl;
+		std::cout << "Your options: \""
+		<< GREEN << "ADD" << ", "
+		<< YELLOW<< " SEARCH" <<", "
+		<< RED << "EXIT"
+		<< END<< "\"" << std::endl;
 		std::cout << "Please Enter you option: ";
 		std::getline(std::cin, input);
+		if (std::cin.eof())
+			return (0);
 		if (input == "ADD")
 			phoneb.add();
 		else if (input == "SEARCH")
@@ -18,5 +25,7 @@ int main(void)
 			return (0);
 		else
 			std::cout << "Wrong optins!!" << std::endl;
+		if (std::cin.eof())
+			return (0);
 	}
 }
