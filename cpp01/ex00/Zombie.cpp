ae29cb12d8f548f48af64ad364eee12aa06f3222
BL4KCY <mostafafarssiui@gmail.com>
2024-07-23 16:26:07 +0100

    refactor: Improve Zombie class implementation

---
+++ cpp01/ex00/Zombie.cpp
@@ -0,0 +1,20 @@
+
+#include "Zombie.hpp"
+void	Zombie::announce(void)
+{
+	std::cout << this->name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
+
+// Constructor
+Zombie::Zombie(std::string name)
+{
+	this->name = name;
+}
+
+// Destructor
+Zombie::~Zombie()
+{
+	std::cout << this->name <<  ": is dead" <<  std::endl;
+}
+
+// end of Zombie.cpp
