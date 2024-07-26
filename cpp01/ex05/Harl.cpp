4cae7164657a9e016e055cf4e33f049ea4b2a050
BL4KCY <mostafafarssiui@gmail.com>
2024-07-26 18:55:04 +0100

    refactor: Update Harl class debug, info, warning, and error methods

--- cpp01/ex05/Harl.cpp
+++ cpp01/ex05/Harl.cpp
@@ -1,6 +1,5 @@
 #include "Harl.hpp"
 
-
 Harl::Harl(void)
 {
 	std::cout << GREEN << "Harl is here!" << RESET << std::endl;
@@ -27,25 +26,25 @@ void Harl::complain(std::string level)
 }
 
 void Harl::debug(void) {
-	std::cout << MAGENTA << "{DEBUG}: " << RESET;
-	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger. I really do!";
+	std::cout << MAGENTA << "{DEBUG}" << RESET << std::endl;
+	std::cout << "I love having extra bacon for my 7XL-double-cheese-triple-pickle-specialketchup burger.\nI really do!";
 	std::cout << std::endl;
 }
 
 void Harl::info(void) {
-	std::cout << GREEN << "{INFO}: " << RESET;
-	std::cout << "I cannot believe adding extra bacon costs more money. You didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
+	std::cout << GREEN << "{INFO}" << RESET << std::endl;
+	std::cout << "I cannot believe adding extra bacon costs more money.\nYou didn’t put enough bacon in my burger! If you did, I wouldn’t be asking for more!";
 	std::cout << std::endl;
 }
 
 void Harl::warning(void) {
-	std::cout << YELLOW << "{WARNING}: " << RESET;
-	std::cout << "I think I deserve to have some extra bacon for free. I’ve been coming for years whereas you started working here since last month.";
+	std::cout << YELLOW << "{WARNING}" << RESET << std::endl;
+	std::cout << "I think I deserve to have some extra bacon for free.\nI’ve been coming for years whereas you started working here since last month.";
 	std::cout << std::endl;
 }
 
 void Harl::error(void) {
-	std::cout << RED << "{ERROR}: " << RESET;
+	std::cout << RED << "{ERROR}" << RESET << std::endl;
 	std::cout << "This is unacceptable! I want to speak to the manager now.";
 	std::cout << std::endl;
 }
