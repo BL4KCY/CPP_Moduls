fc252f20a3a9b959d7e68bcc7c6ae3a6c2c24c37
BL4KCY <mostafafarssiui@gmail.com>
2024-07-31 18:58:18 +0100

    refactor: Add Fixed class implementation and related files

---
+++ cpp02/ex00/Fixed.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <iostream>
+#include <iostream>
+
+class Fixed
+{
+private:
+	int					fixedPoint;
+	static const int	fractionalBits;
+public:
+	Fixed();
+	Fixed( const Fixed &f );
+	~Fixed();
+	Fixed&	operator=( const Fixed &copy );
+	int		getRawBits( void ) const;
+	void	setRawBits( int const raw );
+};
