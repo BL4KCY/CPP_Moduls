d208c64d95bb53db3c567e2b0d811144758d7296
BL4KCY <mostafafarssiui@gmail.com>
2024-08-01 16:43:50 +0100

    refactor: Implement Fixed class with constructors, destructor, assignment operator, and getter/setter methods

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,43 @@
+#include "Fixed.hpp"
+
+// constructor
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	this->fixedPoint = 0;
+}
+
+// copy constructor
+Fixed::Fixed( const Fixed &f )
+{
+	std::cout << "Copy constructor called" << std::endl;
+	this->fixedPoint = f.fixedPoint;
+}
+
+// destructor
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+// assignment operator
+Fixed& Fixed::operator=( const Fixed &copy )
+{
+	std::cout << "Copy assignment operator called" << std::endl;
+	(void)copy;
+	return (*this);
+}
+
+// raw bits getter
+int		Fixed::getRawBits( void ) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (fixedPoint);
+}
+
+// raw bits setter
+void	Fixed::setRawBits( int const raw )
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	this->fixedPoint = raw;
+}
