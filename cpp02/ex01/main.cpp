d0964789b25306098f1f9562fbd9cd9faa46e98b
BL4KCY <mostafafarssiui@gmail.com>
2024-08-04 11:36:35 +0100

    refactor: Add additional tests for Fixed class and update main.cpp

--- cpp02/ex01/main.cpp
+++ cpp02/ex01/main.cpp
@@ -7,6 +7,7 @@ int main( void ) {
 	Fixed const	d( b );
 
 	a = Fixed( 1234.4321f );
+	std::cout << "_________________subject tests_________________" << std::endl;
 	std::cout << "a is " << a << std::endl;
 	std::cout << "b is " << b << std::endl;
 	std::cout << "c is " << c << std::endl;
@@ -15,5 +16,24 @@ int main( void ) {
 	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
 	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
 	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	// my test
+	std::cout << "_________________my tests_________________" << std::endl;
+	float
+	f1  = 5.6f,
+	f2  = 1.125f,
+	f3  = 10.625f;
+
+	std::cout << "_____________ toInt tests _____________" << std::endl;
+	std::cout << "f1: " << f1 << ", toInt: " << Fixed(f1).toInt() << std::endl;
+	std::cout << "f2: " << f2 << ", toInt: " << Fixed(f2).toInt() << std::endl;
+	std::cout << "f3: " << f3 << ", toInt: " << Fixed(f3).toInt() << std::endl;
+
+	std::cout << "_____________ toFloat tests _____________" << std::endl;
+	std::cout << "f1: " << f1 << ", toFloat: " << Fixed(f1).toFloat() << std::endl;
+	std::cout << "f2: " << f2 << ", toFloat: " << Fixed(f2).toFloat() << std::endl;
+	std::cout << "f3: " << f3 << ", toFloat: " << Fixed(f3).toFloat() << std::endl;
+
+
 	return 0;
 }
