3e04335bc8e1550ba14c915207076edcafaf9675
BL4KCY <mostafafarssiui@gmail.com>
2024-08-08 18:49:22 +0100

    refactor: Add ScavTrap class implementation and update main.cpp for new functionality

--- cpp03/ex01/main.cpp
+++ cpp03/ex01/main.cpp
@@ -1,25 +1,12 @@
-#include "ClapTrap.hpp"
-
-
-
+#include "ScavTrap.hpp"
 
 
 
 int main(void)
 {
-	ClapTrap claptrap("Teo");
-	ClapTrap claptrap2(claptrap);
-	ClapTrap claptrap3 = claptrap;
-	ClapTrap claptrap4;
-	claptrap4 = claptrap;
+	ScavTrap scavTrap("jojo");
+	scavTrap.guardGate();
 
-	std::cout << std::endl;
-	std::cout << std::endl;
 
-	claptrap.attack("enemy");
-	claptrap.takeDamage(9);
-	claptrap.attack("enemy");
-	claptrap.beRepaired(10);
-	claptrap.attack("enemy");
 	return (0);
 }
