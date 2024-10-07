7e09b090844571c6b8546349739457feab2bdffa
BL4KCY <mostafafarssiui@gmail.com>
2024-10-07 18:41:19 +0100

    feat: Update main to include new Form instances and Bureaucrat tests for signing functionality

--- C++ - 05 Module/ex01/main.cpp
+++ C++ - 05 Module/ex01/main.cpp
@@ -5,32 +5,71 @@ int main()
 {
 	try
 	{
-		Form	war9a("chahada l7ayat", 10, 20);
-		std::cout << war9a;
+		Form	Feedback("Foodback 1337", 10, 20);
+		Form	Order("speaker Order", 49, 40);
+		Bureaucrat hassan("hassan", 1);
+		Bureaucrat benMoussa("benMossa", 150);
+		Bureaucrat ahmed("ahmed", 50);
+		std::cout << "\t\t**" << Feedback.getName() << " Form**\n" << Feedback;
+		std::cout << "\t\t**" << Order.getName() << " Form**\n" << Order;
+		
+		// testing when the grade of the bureaucrat is high enough to sign the form
 		try
 		{
-			std::cout << "-----------| test hassan |--------------" << std::endl;
-			Bureaucrat hassan("hassan", 1);
+			std::cout << "\t\t**| test hassan |**" << std::endl;
 			std::cout << hassan;
-			hassan.signForm(war9a);
+			hassan.signForm(Feedback);
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << '\n';
 		}
+		std::cout << "\t\t**" << Feedback.getName() << " Form**\n" << Feedback;
+		
+		// testing when the grade of the bureaucrat is not high enough to sign the form
 		try
 		{
-			std::cout << "-----------| test benMossa |--------------" << std::endl;
-			Bureaucrat benMoussa("benMossa", 150);
+			std::cout << "\t\t**| test benMoussa |**" << std::endl;
 			std::cout << benMoussa;
-			benMoussa.signForm(war9a);
+			benMoussa.signForm(Feedback);
 		}
 		catch(const std::exception& e)
 		{
 			std::cerr << e.what() << '\n';
 		}
-		Form	wrong_war9a("chahada 3ozoba", 25, 160);
-		std::cout << wrong_war9a; // should not be printed because of exception (GradeTooLowException)
+		std::cout << "\t\t**" << Feedback.getName() << " Form**\n" << Feedback;
+
+		// testing when the grade of the bureaucrat is not high enough to sign the form
+		try
+		{
+			std::cout << "\t\t**| test ahmed {1}|**" << std::endl;
+			std::cout << ahmed;
+			ahmed.signForm(Order);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << "\t\t**" << Order.getName() << " Form**\n" << Order;
+
+		// testing after incrementing the grade of the bureaucrat to be able to sign the form
+		try
+		{
+			std::cout << "\t\t**| test ahmed {2}|**" << std::endl;
+			ahmed.incrementGrade();
+			std::cout << ahmed;
+			ahmed.signForm(Order);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+		std::cout << "\t\t**" << Order.getName() << " Form**\n" << Order;
+		std::cout << std::endl << "------| end of tests |--------\n" << std::endl;
+
+		// testing when constructing a form with a grade that is too high
+		Form	wrong_Feedback("chahada 3ozoba", 25, 160);
+		std::cout << wrong_Feedback; // should not be printed because of exception (GradeTooLowException)
 	}
 	catch(const std::exception& e)
 	{
