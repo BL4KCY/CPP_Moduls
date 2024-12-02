d8d00863bac4cad3b6f1bec4ab103a27bbde0f94
bl4kcy <mostafafarssiui@gmail.com>
2024-12-02 18:25:04 +0100

    feat: Add additional tests for MutantStack class in main.cpp

--- C++ - 08 Module/ex02/main.cpp
+++ C++ - 08 Module/ex02/main.cpp
@@ -38,6 +38,35 @@ int main()
 		std::cout << *rit << std::endl;
 		++rit;
 	}
+	
+	std::cout << "more tests" << std::endl;
+	MutantStack<int> mstack2;
+	mstack2.push(1);
+	mstack2.push(2);
+	mstack2.push(3);
+	mstack2.push(4);
+	mstack2.push(5);
+	mstack2.push(6);
+	mstack2.push(7);
 
+	MutantStack<int>::iterator it2 = mstack2.begin();
+	MutantStack<int>::iterator ite2 = mstack2.end();
+	++it2;
+	--it2;
+	while (it2 != ite2)
+	{
+		std::cout << *it2 << std::endl;
+		++it2;
+	}
+	std::cout << "Reverse iterator" << std::endl;
+	MutantStack<int>::reverse_iterator rit2 = mstack2.rbegin();
+	MutantStack<int>::reverse_iterator rite2 = mstack2.rend();
+	++rit2;
+	--rit2;
+	while (rit2 != rite2)
+	{
+		std::cout << *rit2 << std::endl;
+		++rit2;
+	}
 	return 0;
 }
