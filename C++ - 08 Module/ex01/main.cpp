f8cb4d8111865c00e7a9d0143b38fc7a207b8fed
bl4kcy <mostafafarssiui@gmail.com>
2024-12-02 12:45:01 +0100

    feat: Implement main function for Span class with basic functionality and exception handling

---
+++ C++ - 08 Module/ex01/main.cpp
@@ -0,0 +1,37 @@
+#include "Span.hpp"
+
+
+
+
+
+int main()
+{
+	Span sp = Span(5);
+	sp.addNumber(6);
+	sp.addNumber(3);
+	sp.addNumber(17);
+	sp.addNumber(9);
+	sp.addNumber(11);
+	std::cout << sp.shortestSpan() << std::endl;
+	std::cout << sp.longestSpan() << std::endl;
+
+	try
+	{
+		Span sp2(100000000);
+		std::deque<int> vec;
+		for (size_t i = 0; i <= 10000000;i++)
+			vec.push_back(i);
+		sp2.addNumbers(vec.begin(), vec.end());
+		std::cout << sp2.shortestSpan() << std::endl;
+		std::cout << sp2.longestSpan() << std::endl;
+		
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+	}
+	
+	//	Test adding multiple numbers
+
+	return 0;
+}
