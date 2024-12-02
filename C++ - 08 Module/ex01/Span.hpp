7215d42f63e76a23fec47c8014173a9adc50c405
bl4kcy <mostafafarssiui@gmail.com>
2024-12-02 12:45:31 +0100

    feat: Add Span class definition with constructors, methods for adding numbers, and calculating spans

---
+++ C++ - 08 Module/ex01/Span.hpp
@@ -0,0 +1,41 @@
+#ifndef SPAN_HPP
+# define SPAN_HPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+#include <vector>
+#include <set>
+#include <list>
+#include <queue>
+#include <algorithm>
+#include <iterator>
+#include <exception>
+#include <limits>
+
+class Span
+{
+private:
+	std::set<int>		intSet;
+	unsigned int		maxSize;
+public:
+	Span();
+	Span( unsigned int N );
+	Span( const Span& cp );
+	Span&	operator = ( Span& cp );
+	~Span();
+	unsigned int getMaxSize( void ) const;
+	std::set<int>	getIntSet( void ) const;
+	void	addNumber( const int _number );
+	unsigned int	shortestSpan( void ) const;
+	unsigned int	longestSpan( void ) const;
+	template <typename Iterator>
+	void	addNumbers(Iterator begin, Iterator end)
+	{
+		if (std::distance(begin, end) >  static_cast<unsigned int>(maxSize - intSet.size())) {
+			throw std::overflow_error("Span: Not enough space in Span to add all numbers!");
+		}
+		intSet.insert(begin, end);
+	}
+};
+
+#endif // Span_HPP
