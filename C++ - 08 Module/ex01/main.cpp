9a642f019fcf512275609c7a7caa9d79d42190d6
bl4kcy <mostafafarssiui@gmail.com>
2024-12-03 02:21:04 +0100

    refactor: Rearrange code in main.cpp for improved readability and maintainability

--- C++ - 08 Module/ex01/main.cpp
+++ C++ - 08 Module/ex01/main.cpp
@@ -6,17 +6,17 @@
 
 int main()
 {
-	Span sp = Span(5);
-	sp.addNumber(6);
-	sp.addNumber(3);
-	sp.addNumber(17);
-	sp.addNumber(9);
-	sp.addNumber(11);
-	std::cout << sp.shortestSpan() << std::endl;
-	std::cout << sp.longestSpan() << std::endl;
-
 	try
 	{
+		Span sp = Span(5);
+		sp.addNumber(6);
+		sp.addNumber(3);
+		sp.addNumber(17);
+		sp.addNumber(9);
+		sp.addNumber(11);
+		std::cout << sp.shortestSpan() << std::endl;
+		std::cout << sp.longestSpan() << std::endl;
+
 		Span sp2(100000000);
 		std::deque<int> vec;
 		for (size_t i = 0; i <= 10000000;i++)
