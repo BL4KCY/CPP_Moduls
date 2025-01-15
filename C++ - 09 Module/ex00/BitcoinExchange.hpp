5f064bdf228e1344e4f9517173491969933f802b
bl4kcy <mostafafarssiui@gmail.com>
2025-01-15 11:12:11 +0100

    fix: Correct include directives in BitcoinExchange.hpp for proper header usage

--- C++ - 09 Module/ex00/BitcoinExchange.hpp
+++ C++ - 09 Module/ex00/BitcoinExchange.hpp
@@ -1,7 +1,23 @@
 #ifndef BITCOINEXCHANGE_HPP
 # define BITCOINEXCHANGE_HPP
-# include <iostream.hpp>
+# include <iostream>
+# include <iomanip>
+# include <string>
+# include <vector>
+# include <map>
+# include <fstream>
+# include <exception>
+# include <sstream>
 
-
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::map;
+using std::istringstream;
 
 #endif // BITCOINEXCHANGE_HPP
