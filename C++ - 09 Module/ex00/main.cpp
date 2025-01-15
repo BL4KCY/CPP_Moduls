f47ed32319d272ab42a42c0f6e282b9be75b16c0
bl4kcy <mostafafarssiui@gmail.com>
2025-01-15 12:33:46 +0100

    feat: Implement input file handling and validation in main function for BitcoinExchange

--- C++ - 09 Module/ex00/main.cpp
+++ C++ - 09 Module/ex00/main.cpp
@@ -3,18 +3,21 @@
 
 void	init_database(map<string, float> &dataBase, ifstream &dataFile);
 float	stof(string str);
+void	action(map<string, float> &dataBase, ifstream &inputFile);
 
 int main( int c, char **av ) {
 	try {
 		if (2 != c)
 			throw std::logic_error("Error: could not open file.");
-		ifstream	dataFile( av[1] );
-		if (dataFile.fail())
+		ifstream	dataFile( "data.csv" );
+		ifstream	inputFile( av[1] );
+		if (dataFile.fail() | inputFile.fail())
 			throw std::logic_error("Error: could not open file.");
 		map<string, float>	dataBase;
 		try
 		{
 			init_database(dataBase, dataFile);
+			action(dataBase, inputFile);
 		}
 		catch(const std::exception& e)
 		{
@@ -27,6 +30,19 @@ int main( int c, char **av ) {
 		return 1;
 	}
 	return 0;
+}
+
+void	action(map<string, float> &dataBase, ifstream &inputFile)
+{
+	string	line;
+	std::getline(inputFile, line);
+	if (line != "date | value")
+		throw std::logic_error("invalid input file please use \'date | value\' format !");
+	while (std::getline(inputFile, line))
+	{
+		
+	}
+	(void) dataBase;
 }
 
 void	init_database(map<string, float> &dataBase, ifstream &dataFile)
