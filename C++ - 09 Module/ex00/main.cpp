7e5f08a71b93da09cbc50ef2dcf3d9160e16b68f
bl4kcy <mostafafarssiui@gmail.com>
2025-01-15 11:11:57 +0100

    feat: Enhance main function with error handling and database initialization for BitcoinExchange

--- C++ - 09 Module/ex00/main.cpp
+++ C++ - 09 Module/ex00/main.cpp
@@ -1,11 +1,47 @@
 #include "BitcoinExchange.hpp"
 
 
-
+void	init_database(map<string, float> &dataBase, ifstream &dataFile);
+float	stof(string str);
 
 int main( int c, char **av ) {
-
-	if (c != 2)
-		cout << 
+	try {
+		if (2 != c)
+			throw std::logic_error("Error: could not open file.");
+		ifstream	dataFile( av[1] );
+		if (dataFile.fail())
+			throw std::logic_error("Error: could not open file.");
+		map<string, float>	dataBase;
+		try
+		{
+			init_database(dataBase, dataFile);
+		}
+		catch(const std::exception& e)
+		{
+			std::cerr << e.what() << '\n';
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << '\n';
+		return 1;
+	}
 	return 0;
+}
+
+void	init_database(map<string, float> &dataBase, ifstream &dataFile)
+{
+	string	line;
+	while (std::getline(dataFile, line))
+	{
+		dataBase[(line.substr(0 ,line.find_first_of(',')))] = stof(line.substr(line.find_first_of(',') + 1));
+	}
+}
+
+float	stof(string str)
+{
+	float	value;
+	istringstream	iss(str);
+	iss >> value;
+	return value;
 }
