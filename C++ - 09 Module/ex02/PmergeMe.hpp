1971ece65a6fb874a87965e5d79da272eb2ae3a0
bl4kcy <mostafafarssiui@gmail.com>
2025-01-31 09:52:18 +0100

    feat: Add header file PmergeMe.hpp with necessary includes and guards

---
+++ C++ - 09 Module/ex02/PmergeMe.hpp
@@ -0,0 +1,33 @@
+#ifndef PMERGEME_HPP
+# define PMERGEME_HPP
+# include <iostream>
+# include <iomanip>
+# include <utility>
+# include <string>
+# include <vector>
+# include <map>
+# include <fstream>
+# include <exception>
+# include <sstream>
+# include <utility>
+# include <cstdlib>
+# include <algorithm>
+
+using std::cout;
+using std::cin;
+using std::cerr;
+using std::endl;
+using std::string;
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::map;
+using std::istringstream;
+using std::pair;
+using std::find_if;
+using std::for_each;
+
+
+
+
+#endif // PMERGEME_HPP
