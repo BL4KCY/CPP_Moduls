c13e9c7bd7114c7bcafed3c2f249ff6f0559d685
bl4kcy <mostafafarssiui@gmail.com>
2025-02-04 09:25:22 +0100

    feat: Refactor main function to include error handling and update sorting class usage

--- C++ - 09 Module/ex02/main.cpp
+++ C++ - 09 Module/ex02/main.cpp
@@ -1,11 +1,18 @@
-#include "PmergeMe.hpp"
-#include "mergeInsertSort.hpp"
+#include "PmergeMe.tpp"
+
+void	putnbr(int nbr)
+{
+	cout << nbr << " ";
+}
 
 int main( int c, char **av ) {
 	vector<int>	vec;
 	deque<int>	deq;
 	
 	try {
+		if (c < 2) {
+			throw std::logic_error("No arguments!!");
+		}
 		for (int i = 1; i < c;i++)
 		{
 			if (string(av[i]).find_first_not_of("0123456789") != string::npos ){ //check if arg is positive number
@@ -17,10 +24,10 @@ int main( int c, char **av ) {
 		cout << "before: ";
 		for_each(vec.begin(), vec.end(), putnbr);
 		cout << endl;
-		mergeInsertSort<vector<int> > sortVec(vec);
-		mergeInsertSort<deque<int> > sortDeq(deq);
+		PmergeMe<vector<int> > sortVec(vec);
+		PmergeMe<deque<int> > sortDeq(deq);
 		cout << "after :";
-		for_each(sortVec.getarr().begin(), sortVec.getarr().end(), putnbr);
+		for_each(sortVec.getArr().begin(), sortVec.getArr().end(), putnbr);
 		cout << endl;
 		cout << std::fixed << std::setprecision(5);
 		cout << "Time to process a range of " << vec.size() << " elements with std::vector<int> : " << sortVec.timeTakes() << " seconds" << endl;
